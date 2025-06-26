1e384c63a904512955eb76b7f1059d3e88662464
dparmar <116805076+dparmar04@users.noreply.github.com>
2025-06-26 12:04:54 +0530

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
       <div
         className="absolute z-10 text-white px-4 w-full
         flex flex-col items-center text-center md:items-center md:text-center lg:items-start lg:text-left lg:w-auto top-[90%] left-1/2 transform -translate-x-1/2 -translate-y-1/2
-        lg:top-auto lg:bottom-10 lg:left-1/3 lg:transform-none
+        lg:top-auto lg:bottom-10 lg:left-1/4 md:left-1/3 lg:transform-none
         max-w-[90%] md:max-w-[80%] lg:max-w-none"
       >
         <h1 className="text-4xl md:text-5xl lg:text-7xl font-medium leading-tight mb-3">
